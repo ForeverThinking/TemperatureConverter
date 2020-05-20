7c102d7937dff132d19c73b7597cf8cf85a96470
ForeverThinking <35239092+ForeverThinking@users.noreply.github.com>
2020-05-20 15:14:07 +0100

    Connect Main and Temperatures classes. Change from static methods to switch statement.

--- Main.cs
+++ Main.cs
@@ -6,7 +6,40 @@ namespace TemperatureConverter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Temperatures.CelciusToFahrenheit(3));
+            StartProgram();
+        }
+
+        private static void StartProgram()
+        {
+            Console.Clear();
+            Console.WriteLine("Which of the following do you require?");
+            Console.WriteLine("1. Celcius to Fahrenheit");
+            Console.WriteLine("2. Celcius to Kelvin");
+            Console.WriteLine("3. Fahrenheit to Celcius");
+            Console.WriteLine("4. Fahrenheit to Kelvin");
+            Console.WriteLine("5. Kelvin to Celcius");
+            Console.WriteLine("6. Kelvin to Fahrenheit");
+            Console.Write("Please enter the appropriate number: ");
+            string fromChoice = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.Write("What is the value you are converting from: ");
+            string fromValue = Console.ReadLine();
+            float valueAsFloat;
+
+            Console.Clear();
+
+            if (float.TryParse(fromValue, out valueAsFloat))
+            {
+                Temperatures tempConverter = new Temperatures(fromChoice, valueAsFloat);
+                tempConverter.DoConversion();
+                Console.WriteLine($"Your converted temperature is {tempConverter.ConvertedTempResult}.");
+            }
+            else
+            {
+                Console.WriteLine("The value given could not be converted.");
+            }
         }
     }
 }
--- Temperatures.cs
+++ Temperatures.cs
@@ -1,39 +1,51 @@
 namespace TemperatureConverter
 {
     /// <summary>
-    /// Contains static methods to convert between temperature scales
+    /// Contains static methods to convert between temperature scales.
     /// Float chosen as data types as accuracy not needed for this app.
     /// </summary>
     public class Temperatures
     {
-        public static float CelciusToFahrenheit(float celcius)
+        private string choiceMade;
+        private float fromTemp;
+        private float convertedTempResult;
+        public float ConvertedTempResult 
         {
-            return ((celcius * 9/5) + 32); // Temp in Fahrenheit
+            get
+            {
+                return convertedTempResult;
+            }
         }
 
-        public static float CelciusToKelvin(float celcius)
+        public Temperatures(string selectedChoice, float fromTemperature)
         {
-            return celcius + 273.15f; // Temp in Kelvin
+            this.choiceMade = selectedChoice;
+            this.fromTemp = fromTemperature;
         }
 
-        public static float FahrenheitToCelcius(float fahrenheit)
+        public void DoConversion()
         {
-            return ((fahrenheit -32) * 5/9); // Temp in Celcius
-        }
-
-        public static float FahrenheitToKelvin(float fahrenheit)
-        {
-            return ((fahrenheit + 459.67f) * (5/9)); // Temp in kelvin
-        }
-
-        public static float KelvinToCelcius(float kelvin)
-        {
-            return kelvin - 273.15f; // Temp in Celcius
-        }
-
-        public static float KelvinToFahrenheit(float kelvin)
-        {
-            return ((kelvin * 9/5) - 459.67f); // Temp in fahrenheit
+            switch (choiceMade)
+            {
+                case "1": // celcius to fahrenheit
+                    convertedTempResult = ((fromTemp * 9/5) + 32);
+                    break;
+                case "2": // celcius to kelvin
+                    convertedTempResult = fromTemp + 273.15f;
+                    break;
+                case "3": // fahrenheit to celsius
+                    convertedTempResult = ((fromTemp -32) * 5/9);
+                    break;
+                case "4": // fahrenheit to kelvin
+                    convertedTempResult = ((fromTemp + 459.67f) * (5/9));
+                    break;
+                case "5": // kelvin to celsius
+                    convertedTempResult = fromTemp - 273.15f;
+                    break;
+                case "6": // kelvin to fahrenheit
+                    convertedTempResult = ((fromTemp * 9/5) - 459.67f);
+                    break;
+            }
         }
     }
 }
